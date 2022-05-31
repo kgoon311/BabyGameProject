b2eb772e5cb3e9fd9e4985ccdaa85c1119147bf1
kgoon <sdh210203@sdh.hs.kr>
2022-05-31 20:49:27 +0900

    title

--- Assets/script/Title/TitleManager.cs
+++ Assets/script/Title/TitleManager.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class TitleManager : MonoBehaviour
 {
-    private bool ScreenTouch;
+    [Header("타이틀")]
+    [SerializeField] GameObject Title;
     [SerializeField] GameObject ClickText;
+    public bool ScreenTouch;
+
+    [Header("스테이지")]
+    public int LastClearStage;
+    [SerializeField] GameObject StagePanel;
+    [SerializeField] GameObject StageGroup;
     void Start()
     {
+        Title.transform.DOScale(Vector3.one, 1).SetEase(Ease.InQuint);
+        for (int i = 0; i <= LastClearStage;i++)
+        {
+            StageGroup.transform.GetChild(i).GetChild(0).gameObject.SetActive(false);
+        }
         StartCoroutine(TextMove());
     }
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (ScreenTouch == false)
+            {
+                ScreenTouch = true;
+                StagePanel.transform.DOLocalMove(Vector3.zero, 1f, false).SetEase(Ease.OutBounce);
+            }
+        }
     }
     private IEnumerator TextMove()
     {
@@ -23,16 +43,8 @@ public class TitleManager : MonoBehaviour
         yield return new WaitForSeconds(2f);
         yield return StartCoroutine(TextMove());
     }
-    private void OnMouseDown()
+    public void ClickShow(int stage)
     {
-        if (ScreenTouch == false)
-        {
-            StartCoroutine(ClickShow());
-        }
-    }
-    private IEnumerator ClickShow()
-    {
-
-        yield return null;
+        SceneManager.LoadScene(stage);
     }
 }
